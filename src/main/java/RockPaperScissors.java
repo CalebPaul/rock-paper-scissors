484a12c579b3b8736e18197883c5453e551e2f2e
Caleb Paul and Caleb Stevenson <caleb@>
2016-09-07 16:41:38 -0700

    add player two winning conditions and passing tests, begin tie conditions

--- src/main/java/RockPaperScissors.java
+++ src/main/java/RockPaperScissors.java
@@ -10,4 +10,26 @@ public class RockPaperScissors {
     }
     return result;
   }
+
+  public Boolean playerTwoWins(String twoChoice, String oneChoice) {
+    Boolean result = false;
+    if (twoChoice.equalsIgnoreCase("rock") && oneChoice.equalsIgnoreCase("scissors")) {
+      result = true;
+    } else if (twoChoice.equalsIgnoreCase("paper") && oneChoice.equalsIgnoreCase("rock")) {
+      result = true;
+    } else if (twoChoice.equalsIgnoreCase("scissors") && oneChoice.equalsIgnoreCase("paper")) {
+      result = true;
+    }
+    return result;
+  }
+
+  public Boolean playerTie(String oneChoice, String twoChoice) {
+    Boolean result = false;
+    if (oneChoice.equalsIgnoreCase("rock") && twoChoice.equalsIgnoreCase("rock")) {
+      result = true;
+    } /*else if (oneChoice.equalsIgnoreCase("scissors") && twoChoice.equalsIgnoreCase("scissors")) {
+      result = true;
+    } */
+    return result;
+  }
 }
--- src/test/java/RockPaperScissorsTest.java
+++ src/test/java/RockPaperScissorsTest.java
@@ -4,24 +4,56 @@ import static org.junit.Assert.*;
 public class RockPaperScissorsTest {
 
   @Test
-  public void playerOneTurn_PlayerOneWinsRock_True() {
+  public void playerOneWins_PlayerOneWinsRock_True() {
     RockPaperScissors testRockPaperScissors = new RockPaperScissors();
     Boolean expected = true;
     assertEquals(expected, testRockPaperScissors.playerOneWins("rock", "scissors"));
   }
 
   @Test
-  public void playerOneTurn_PlayerOneWinsPaper_True() {
+  public void playerOneWins_PlayerOneWinsPaper_True() {
     RockPaperScissors testRockPaperScissors = new RockPaperScissors();
     Boolean expected = true;
     assertEquals(expected, testRockPaperScissors.playerOneWins("paper", "rock"));
   }
 
   @Test
-  public void playerOneTurn_PlayerOneWinsScissors_True() {
+  public void playerOneWins_PlayerOneWinsScissors_True() {
     RockPaperScissors testRockPaperScissors = new RockPaperScissors();
     Boolean expected = true;
     assertEquals(expected, testRockPaperScissors.playerOneWins("scissors", "paper"));
   }
 
+  @Test
+  public void playerTwoWins_PlayerTwoWinsRock_True() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    Boolean expected = true;
+    assertEquals(expected, testRockPaperScissors.playerTwoWins("rock", "scissors"));
+  }
+
+  @Test
+  public void playerTwoWins_PlayerTwoWinsPaper_True() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    Boolean expected = true;
+    assertEquals(expected, testRockPaperScissors.playerTwoWins("paper", "rock"));
+  }
+
+  @Test
+  public void playerTwoWins_PlayerTwoWinsScissors_True() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    Boolean expected = true;
+    assertEquals(expected, testRockPaperScissors.playerTwoWins("scissors", "paper"));
+  }
+
+  @Test
+  public void playerTie_playerTieRock_True() {
+    RockPaperScissors testRockPaperScissors = new RockPaperScissors();
+    Boolean expected = true;
+    assertEquals(expected, testRockPaperScissors.playerTie("rock", "rock"));
+  }
+
+  @Test
+  public void playerTie_playerTieScissors_True() {
+    Rock
+  }
 }
